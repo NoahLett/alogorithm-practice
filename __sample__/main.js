9913e04378fc419825ed207ec10d29085a0ee546
Noah Lett <noahblett96@gmail.com>
2022-11-23 22:43:20 +0000

    javascript-algo-century solution - Noah Lett

--- __sample__/main.js
+++ __sample__/main.js
@@ -1,1 +1,23 @@
-console.log('Hello, Code Solutions!');
+
+// Find the century of a given year //
+
+function solution(year) {
+  var divided = year / 100;
+  if (divided < 1) {
+    return 1;
+  } else if (divided === 1) {
+    return 1;
+  } else if (Number.isInteger(divided) && divided > 1) {
+    return divided;
+  } else if (!Number.isInteger(divided) && divided > 1) {
+    return Math.floor(divided) + 1;
+  }
+}
+
+console.log(
+  solution(1905),
+  solution(1700),
+  solution(1988),
+  solution(2000),
+  solution(8),
+  solution(374));
